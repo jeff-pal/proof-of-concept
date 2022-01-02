89a72a07835cf33fb6bcbc8d1f084cb5b32ccb83
jeffpal <jeffpal.official@gmail.com>
2022-01-01 21:38:25 -0300

    chore: start the api app
    Create an application structure and config Typescript and Jest

---
+++ thumbnail-generator-api/app/jest.config.ts
@@ -0,0 +1,27 @@
+export default {
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: true,
+
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  // An array of glob patterns indicating a set of files for which coverage information should be collected
+  collectCoverageFrom: [
+    "<rootDir>/src/**/*.ts",
+    "<rootDir>/src/**/*.js",
+  ],
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: "coverage",
+
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: "v8",
+
+  // The test environment that will be used for testing
+  testEnvironment: "jest-environment-node",
+
+  // A list of paths to directories that Jest should use to search for files in
+  roots: [
+    "<rootDir>/src"
+  ],
+};
