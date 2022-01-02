331d17f2bb00dfdb2372980c08c2e5466b0538c8
jeffpal <jeffpal.official@gmail.com>
2022-01-02 13:01:34 -0300

    test: create test for size argument

--- thumbnail-generator-api/app/src/tests/image.test.ts
+++ thumbnail-generator-api/app/src/tests/image.test.ts
@@ -1,10 +1,13 @@
 import Image from '../adapters/imageV1';
+import imageTypes from '../types/imageTypes';
 
 describe('Image Tests', () => {
+    const size: imageTypes.Resolution = { width: 100, height: 100 }
+
     test("Should return a Buffer", () => {
         const image = new Image();
         const imageBuffer = Buffer.from('test image buffer');
-        const resizedImage = image.resize(imageBuffer);
+        const resizedImage = image.resize(imageBuffer, size);
 
         expect(resizedImage).toBeInstanceOf(Buffer);
     })
@@ -12,7 +15,7 @@ describe('Image Tests', () => {
     test("Should return an Error if the file buffer is empty.", () => {
         const image = new Image();
         const imageBuffer = Buffer.alloc(0);
-        const resizedImage = image.resize(imageBuffer);
+        const resizedImage = image.resize(imageBuffer, size);
 
         expect(resizedImage).toEqual(new Error('The file buffer is empty'));
     })
@@ -20,8 +23,17 @@ describe('Image Tests', () => {
     test("Should return an Error if the file buffer is not a Buffer instance of.", () => {
         const image = new Image();
         // @ts-ignore - with this command we ignore type checking for the line bellow.
-        const resizedImage = image.resize();
+        const resizedImage = image.resize(null, size);
 
         expect(resizedImage).toEqual(new Error('Invalid file format. The file is not a Buffer.'));
+    })
+
+    test("Should return an Error if the size(image dimension) is invalid.", () => {
+        const image = new Image();
+        const imageBuffer = Buffer.from('test image buffer');
+        // @ts-ignore - with this command we ignore type checking for the line bellow.
+        const resizedImage = image.resize(imageBuffer, null);
+
+        expect(resizedImage).toEqual(new Error('Invalid image resolution.'));
     })
 });
