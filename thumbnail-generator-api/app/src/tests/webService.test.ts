46a2cf9ce88354f599f6e937123c7aa92c791ec3
jeffpal <jeffpal.official@gmail.com>
2022-01-03 21:41:06 -0300

    test: write web service tests

---
+++ thumbnail-generator-api/app/src/tests/webService.test.ts
@@ -0,0 +1,31 @@
+import webService from '../../webService';
+import supertest from 'supertest';
+
+jest.mock('express-fileupload', () => {
+    return () => (request, response, next) => {
+        request.files = { 
+            image_to_be_resized: {
+                name: 'test.png',
+                mimetype: 'image/png',
+                data: Buffer.from('test')
+            }
+        }
+        next();
+    }
+})
+
+jest.mock('../adapters/awsS3', () => {
+  return jest.fn(() => ({
+      upload: () => 'link'
+  }))  
+});
+
+jest.mock('resize-image-buffer')
+
+describe('Test Web Service', () => {
+    test('Should return resized images array', async () => {
+        const app = webService.app;
+        const response = await supertest(app).post("/resize-image");
+        expect(typeof response.body).toBe('object')
+    })
+})
