e0624ccc7433789c19f6b28eee37ea7a2a739da6
jeffpal <jeffpal.official@gmail.com>
2022-01-06 01:41:36 -0300

    chore: create route to test app

--- thumbnail-generator-api/app/src/adapters/thumbnailRoutes.ts
+++ thumbnail-generator-api/app/src/adapters/thumbnailRoutes.ts
@@ -7,5 +7,8 @@ const resizeImageRouter = Router()
 resizeImageRouter.post('/resize-to-three-dimensions', resizeImageController.handle.bind(resizeImageController))
 
 thumbnailGeneratorRouter.use('/thumbnail', resizeImageRouter)
+thumbnailGeneratorRouter.get('/', async (req, res, next) => {
+      res.status(200).send('Thumbnail app is running...')
+    })
 
 export default thumbnailGeneratorRouter;
