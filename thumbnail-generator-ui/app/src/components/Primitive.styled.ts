da63326d225df0600f1a68de2c06877029d1409b
jeffpal <jeffpal.official@gmail.com>
2022-01-09 20:22:16 -0300

    chore: create primitive styled components

---
+++ thumbnail-generator-ui/app/src/components/Primitive.styled.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 20px;
+`;
+
+export {
+  Column
+};
--- thumbnail-generator-ui/app/src/components/index.ts
+++ thumbnail-generator-ui/app/src/components/index.ts
@@ -2,3 +2,4 @@ export * from './DefaultDragAndDropContent';
 export * from './DragAndDropFile';
 export * from './LoginButton';
 export * from './LogoutButton';
+export * from './Primitive.styled';
